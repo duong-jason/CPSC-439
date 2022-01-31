26155a7108c284e31cefa5770a8ec970cafa77aa
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-01-30 18:19:25 -0800

    retabbed!

--- notes/chapter_1/Multiply_1.c
+++ notes/chapter_1/Multiply_1.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-	int x, y, z = 0;
+    int x, y, z = 0;
 
-	if (argc == 3 && (x = atoi(argv[1])) && (y = atoi(argv[2]))) {
-		for (int i = 0; i < y; ++i)
-			z += x;
-		printf("%d * %d = %d\n", x, y, z);
-	}
+    if (argc == 3 && (x = atoi(argv[1])) && (y = atoi(argv[2]))) {
+        for (int i = 0; i < y; ++i)
+            z += x;
+        printf("%d * %d = %d\n", x, y, z);
+    }
 }
--- notes/chapter_1/Multiply_2.cpp
+++ notes/chapter_1/Multiply_2.cpp
@@ -5,7 +5,7 @@
 #include <math.h>
 
 int main(int argc, char** argv) {
-	if (argc == 3) {
+    if (argc == 3) {
         int x = atoi(argv[1]), y = atoi(argv[2]), i, j, z;
         const size_t n = log10(x) + 1, m = log10(y) + 1;
         int *xptr = new int[n], *yptr = new int[m];
@@ -23,12 +23,12 @@ int main(int argc, char** argv) {
             yt /= 10;
         }
 
-		for (i = 0; i < n; ++i) {
+        for (i = 0; i < n; ++i) {
             for (j = 0; j < m; ++j) {
                 z += pow(10, i+j) * xptr[i] * yptr[j];
             }
         }
 
-		printf("%d * %d = %d\n", x, y, z);
-	}
+        printf("%d * %d = %d\n", x, y, z);
+    }
 }
